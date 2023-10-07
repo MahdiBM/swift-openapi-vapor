6bf58b0bc5c1a06081a6b2d2323830f87dfa72bb
MahdiBM <github@mahdibm.com>
2023-10-07 08:01:15 +0330

    Update Streaming.swift

--- Sources/OpenAPIVapor/Streaming.swift
+++ Sources/OpenAPIVapor/Streaming.swift
@@ -20,21 +20,21 @@ enum Streaming {
         }
 
         func write() async {
-            print("b do", Thread.current.name, Thread.current.threadPriority, Thread.current.qualityOfService.rawValue, Thread.current.isMainThread, Thread.current.isCancelled, Thread.current.isExecuting, Thread.current.isFinished)
+            print("b do", Thread.current.name)
             do {
-                print("a do", Thread.current.name, Thread.current.threadPriority, Thread.current.qualityOfService.rawValue, Thread.current.isMainThread, Thread.current.isCancelled, Thread.current.isExecuting, Thread.current.isFinished)
+                print("a do", Thread.current.name)
                 for try await chunk in body {
-                    print("in for", Thread.current.name, Thread.current.threadPriority, Thread.current.qualityOfService.rawValue, Thread.current.isMainThread, Thread.current.isCancelled, Thread.current.isExecuting, Thread.current.isFinished)
+                    print("in for", Thread.current.name)
                     try await writer.write(.buffer(ByteBuffer(bytes: chunk))).get()
-                    print("a for", Thread.current.name, Thread.current.threadPriority, Thread.current.qualityOfService.rawValue, Thread.current.isMainThread, Thread.current.isCancelled, Thread.current.isExecuting, Thread.current.isFinished)
+                    print("a for", Thread.current.name)
                 }
-                print("done for", Thread.current.name, Thread.current.threadPriority, Thread.current.qualityOfService.rawValue, Thread.current.isMainThread, Thread.current.isCancelled, Thread.current.isExecuting, Thread.current.isFinished)
+                print("done for", Thread.current.name)
                 try await writer.write(.end).get()
-                print("ended", Thread.current.name, Thread.current.threadPriority, Thread.current.qualityOfService.rawValue, Thread.current.isMainThread, Thread.current.isCancelled, Thread.current.isExecuting, Thread.current.isFinished)
+                print("ended", Thread.current.name)
             } catch {
-                print("b err", Thread.current.name, Thread.current.threadPriority, Thread.current.qualityOfService.rawValue, Thread.current.isMainThread, Thread.current.isCancelled, Thread.current.isExecuting, Thread.current.isFinished)
+                print("b err", Thread.current.name)
                 try? await writer.write(.error(error)).get()
-                print("a err", Thread.current.name, Thread.current.threadPriority, Thread.current.qualityOfService.rawValue, Thread.current.isMainThread, Thread.current.isCancelled, Thread.current.isExecuting, Thread.current.isFinished)
+                print("a err", Thread.current.name)
             }
         }
     }
@@ -71,5 +71,47 @@ enum Streaming {
                 writer.write(.error(error))
             }.get()
         }
+    }
+}
+
+extension EventLoopFuture {
+    /// Get the value/error from an `EventLoopFuture` in an `async` context.
+    ///
+    /// This function can be used to bridge an `EventLoopFuture` into the `async` world. Ie. if you're in an `async`
+    /// function and want to get the result of this future.
+    @available(macOS 10.15, iOS 13, tvOS 13, watchOS 6, *)
+    @inlinable
+    public func getNew() async throws -> Value {
+        print("get b withUnsafe", Thread.current.name)
+        return try await withUnsafeThrowingContinuation { (cont: UnsafeContinuation<UnsafeTransfer<Value>, Error>) in
+            print("get b whenComplete", Thread.current.name)
+            self.whenComplete { result in
+                print("get in whenComplete resume", Thread.current.name)
+                switch result {
+                case .success(let value):
+                    print("get b succ resume", Thread.current.name)
+                    cont.resume(returning: UnsafeTransfer(value))
+                    print("get a succ resume", Thread.current.name)
+                case .failure(let error):
+                    print("get b fail resume", Thread.current.name)
+                    cont.resume(throwing: error)
+                    print("get a fail resume", Thread.current.name)
+                }
+                print("get out whenComplete", Thread.current.name)
+            }
+            print("get a whenComplete", Thread.current.name)
+        }.wrappedValue
+        print("get a withUnsafe", Thread.current.name)
+    }
+}
+
+@usableFromInline
+struct UnsafeTransfer<Wrapped> {
+    @usableFromInline
+    var wrappedValue: Wrapped
+
+    @inlinable
+    init(_ wrappedValue: Wrapped) {
+        self.wrappedValue = wrappedValue
     }
 }
